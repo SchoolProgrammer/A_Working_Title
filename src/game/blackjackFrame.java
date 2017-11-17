ae5662d5c4f4939024b0e511acb7ad6db6f3b95f
cross.william <cross.william@306A-16.charterschool.local>
2017-11-17 09:44:41 -0500

    worked on blackjack

--- src/game/blackjackFrame.java
+++ src/game/blackjackFrame.java
@@ -14,8 +14,12 @@ public class blackjackFrame extends javax.swing.JFrame {
     /**
      * Creates new form blackjack
      */
-    public blackjackFrame(int money) {
-        initComponents(69);
+    public blackjackFrame(int cash) {
+        card[] deck = new card[52];
+        for (int x = 0; x < 52; x ++) {
+            
+        }
+        initComponents(deck);
     }
 
     /**
@@ -25,7 +29,9 @@ public class blackjackFrame extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
-    private void initComponents(int money) {
+    private void initComponents(card[] deck) {
+        
+        
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -105,40 +111,11 @@ public class blackjackFrame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(blackjackFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(blackjackFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(blackjackFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(blackjackFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
+    
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new blackjackFrame(69).setVisible(true);
-            }
-        });
-    }
-
-    // Variables declaration - do not modify                     
+    // Variables declaration - do not modify            
+    private int money;
+    private card[] deck;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
